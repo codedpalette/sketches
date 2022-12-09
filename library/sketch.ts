c91725e13c6e53a44675033d81ed785dc35cab04
Daniel Blinder <daniel.blindr@gmail.com>
2022-12-09 23:46:41 +0200

    Convert glyphs to polygons

--- library/sketch.ts
+++ library/sketch.ts
@@ -1,34 +1,42 @@
 import { Application, Container } from "pixi.js";
 
 export abstract class Sketch2D {
-  private container: Container;
+  private app: Application;
   private _elapsed = 0.0;
 
   protected get elapsed() {
     return this._elapsed;
   }
 
-  abstract setup(): Container;
+  protected get width() {
+    return this.app.renderer.width;
+  }
 
+  protected get height() {
+    return this.app.renderer.height;
+  }
+
+  abstract setup(): Container;
   update(delta: number, container: Container): void {}
 
+  draw(): void {
+    const container = this.setup();
+    container.position = { x: this.width / 2, y: this.height / 2 };
+    this.app.stage.addChild(container);
+
+    this.app.ticker.add((delta) => {
+      this._elapsed += delta;
+      this.update(delta, container);
+    });
+  }
+
   constructor(width = 1080, height = 1080, bgColor: string | number = "white") {
-    const app = new Application({
+    this.app = new Application({
       width,
       height,
       antialias: true,
       backgroundColor: bgColor,
     });
-    document.body.appendChild(app.view as any as Node);
-
-    this.container = this.setup();
-    this.container.position = { x: width / 2, y: height / 2 };
-    this.container.scale = { x: 1, y: -1 };
-    app.stage.addChild(this.container);
-
-    app.ticker.add((delta) => {
-      this._elapsed += delta;
-      this.update(delta, this.container);
-    });
+    document.body.appendChild(this.app.view as any as Node);
   }
 }
--- sketches/whoami.ts
+++ sketches/whoami.ts
@@ -1,13 +1,59 @@
-import { Container, DisplayObject, Graphics } from "pixi.js";
+import { Container, DisplayObject, Graphics, TextStyle, Text } from "pixi.js";
 import { Sketch2D } from "../library/sketch";
+import * as opentype from "opentype.js";
 
 class WhoAmI extends Sketch2D {
-  setup(): Container<DisplayObject> {
+  font: opentype.Font;
+
+  constructor(font: opentype.Font) {
+    super();
+    this.font = font;
+  }
+
+  private parseCommand(command: opentype.PathCommand, graphics: Graphics) {
+    switch (command.type) {
+      case "M":
+        graphics.moveTo(command.x, command.y);
+        break;
+      case "L":
+        graphics.lineTo(command.x, command.y);
+        break;
+      case "C":
+        graphics.bezierCurveTo(
+          command.x1,
+          command.y1,
+          command.x2,
+          command.y2,
+          command.x,
+          command.y
+        );
+        break;
+      case "Q":
+        graphics.quadraticCurveTo(command.x1, command.y1, command.x, command.y);
+        break;
+      case "Z":
+        graphics.closePath();
+        break;
+    }
+  }
+
+  private generateMainText(): Graphics {
+    const glyph = this.font.charToGlyph("Х");
+    const path = glyph.getPath();
     const graphics = new Graphics();
-    graphics.beginFill(0xff0000);
-    graphics.drawCircle(0, 0, 50);
+    graphics.lineStyle(1, 0x0000ff);
+    path.commands.forEach((command) => this.parseCommand(command, graphics));
+    const bounds = graphics.getBounds();
+    graphics.lineStyle(1, 0xff0000);
+    graphics.drawShape(bounds);
     return graphics;
+  }
+
+  setup(): Container<DisplayObject> {
+    return this.generateMainText();
   }
 }
 
-new WhoAmI();
+opentype.load("whoami/StalinistOne-Regular.ttf", (err, font) => {
+  font ? new WhoAmI(font).draw() : console.error(err);
+});
