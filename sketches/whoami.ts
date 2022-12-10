c670538b0585f2942f9707f6ffeb8b7774c5e876
Daniel Blinder <daniel.blindr@gmail.com>
2022-12-10 17:21:08 +0200

    Generating main text is working

--- sketches/whoami.ts
+++ sketches/whoami.ts
@@ -6,11 +6,13 @@ import * as opentype from "opentype.js";
 class WhoAmI extends Sketch2D {
   font: opentype.Font;
   debug: boolean;
+  mainPaths: paper.CompoundPath[];
 
   constructor(font: opentype.Font, debug = false) {
     super();
     this.font = font;
     this.debug = debug;
+    this.mainPaths = [];
     paper.setup([this.width, this.height]);
   }
 
@@ -35,21 +37,16 @@ class WhoAmI extends Sketch2D {
     return boundingBox;
   }
 
-  private generateMainText(firstLine: string, secondLine: string, lineHeight: number, margin: number): Graphics {
-    [firstLine, secondLine].forEach((line, lineIdx) => {
-      const y = lineIdx == 0 ? lineHeight / 2 : -lineHeight / 2;
-      const lineWidth = this.width - 2 * margin;
-      [...line].forEach((char, charIdx) => {});
-    });
-
-    const path = this.glyphToPath(this.font.charToGlyph("Х"));
-    const boundingBox = this.calculateBoundingBox(path);
-    const scale = 300 / boundingBox.height;
-    //path.scale(scale, [0, 0]);
-    path.translate([-boundingBox.width / 2, -boundingBox.height / 2]);
-    path.scale(scale, [0, 0]);
-
+  private drawMainGlyph(x: number, y: number, char: string, lineHeight: number): Graphics {
     const graphics = new Graphics();
+    const path = this.glyphToPath(this.font.charToGlyph(char));
+    const boundingBox = this.calculateBoundingBox(path);
+    const scaleX = lineHeight / boundingBox.width; //TODO: Maybe fix for very wide letters
+    const scaleY = lineHeight / boundingBox.height;
+    path.translate([-boundingBox.width / 2, -boundingBox.height / 2]);
+    path.scale(scaleX, scaleY, [0, 0]);
+    this.mainPaths.push(path);
+
     if (this.debug) {
       graphics.lineStyle(1, 0x00ff00);
       graphics.drawShape(this.calculateBoundingBox(path));
@@ -66,18 +63,41 @@ class WhoAmI extends Sketch2D {
         curve.segment2.point.y
       );
     }
-
-    graphics.position.set(0, 150);
+    graphics.position.set(x, y);
     return graphics;
   }
 
-  private drawBaselines(height: number, margin: number): Graphics {
+  private generateMainText(
+    firstLine: string,
+    secondLine: string,
+    lineHeight: number,
+    lineMargin: number,
+    margin: number
+  ): Container {
+    const mainTextContainer = new Container();
+    const xStart = -this.width / 2 + margin;
+    [firstLine, secondLine].forEach((line, lineIdx) => {
+      const y = lineIdx == 0 ? lineMargin + lineHeight / 2 : -lineMargin - lineHeight / 2;
+      const lineWidth = this.width - 2 * margin;
+      const xStep = lineWidth / line.length;
+      [...line].forEach((char, charIdx) => {
+        const x = xStart + xStep * (charIdx + 0.5);
+        mainTextContainer.addChild(this.drawMainGlyph(x, y, char, lineHeight));
+      });
+    });
+    this.debug && mainTextContainer.addChild(this.drawBaselines(lineHeight, lineMargin, margin));
+    return mainTextContainer;
+  }
+
+  private drawBaselines(lineHeight: number, lineMargin: number, margin: number): Graphics {
     const graphics = new Graphics();
     graphics.lineStyle(1, 0xff0000);
     const lines = [
-      [-this.width / 2, height, this.width / 2, height],
+      [-this.width / 2, lineMargin, this.width / 2, lineMargin],
+      [-this.width / 2, lineMargin + lineHeight, this.width / 2, lineMargin + lineHeight],
       [-this.width / 2, 0, this.width / 2, 0],
-      [-this.width / 2, -height, this.width / 2, -height],
+      [-this.width / 2, -lineMargin, this.width / 2, -lineMargin],
+      [-this.width / 2, -lineMargin - lineHeight, this.width / 2, -lineMargin - lineHeight],
       [-this.width / 2 + margin, this.height / 2, -this.width / 2 + margin, -this.height / 2],
       [0, this.height / 2, 0, -this.height / 2],
       [this.width / 2 - margin, this.height / 2, this.width / 2 - margin, -this.height / 2],
@@ -92,11 +112,11 @@ class WhoAmI extends Sketch2D {
 
   setup(): Container<DisplayObject> {
     const lineHeight = 300;
+    const lineMargin = 50;
     const margin = 10;
 
     const container = new Container();
-    this.debug && container.addChild(this.drawBaselines(lineHeight, margin));
-    container.addChild(this.generateMainText("ХТО", "Я?", lineHeight, margin));
+    container.addChild(this.generateMainText("ХТО", "Я?", lineHeight, lineMargin, margin));
     return container;
   }
 }
