c92a5eaf7716f91e6b2bf0dd45b76ac1782c769a
daniel <daniel.blindr@gmail.com>
2023-01-06 17:23:15 +0200

    Tried pixi simple-mesh

--- library/drawing/sketch.ts
+++ library/drawing/sketch.ts
@@ -4,13 +4,14 @@ import { drawLines, LineLike } from "./helpers";
 
 //TODO: Refactor to functional interface
 export abstract class Sketch2D {
-  protected debug: boolean;
   private app: Application;
-  private fpsContainer: Container;
-  private _elapsed = 0;
 
-  constructor(debug: boolean, width = 1080, height = 1080, bgColor: string | number = "white") {
-    this.debug = debug;
+  constructor(
+    protected readonly debug: boolean,
+    protected readonly width = 1080,
+    protected readonly height = 1080,
+    bgColor: string | number = "white"
+  ) {
     this.app = new Application({
       width,
       height,
@@ -18,21 +19,12 @@ export abstract class Sketch2D {
       backgroundColor: bgColor,
       preserveDrawingBuffer: true,
     });
-    this.fpsContainer = new Container();
     document.body.appendChild(this.app.view as unknown as Node);
     setup([this.width, this.height]);
   }
 
-  protected get elapsed() {
-    return this._elapsed;
-  }
-
-  protected get width() {
-    return this.app.renderer.width;
-  }
-
-  protected get height() {
-    return this.app.renderer.height;
+  protected get renderer() {
+    return this.app.renderer;
   }
 
   draw(): void {
@@ -45,9 +37,7 @@ export abstract class Sketch2D {
 
     this.app.stage.addChild(mainContainer);
     this.app.ticker.add(() => {
-      this.debug && this.updateFPS();
       const delta = this.app.ticker.deltaMS / 1000;
-      this._elapsed += delta;
       this.update(delta);
     });
   }
@@ -65,20 +55,23 @@ export abstract class Sketch2D {
   private drawFPS(): Container {
     const margin = 10;
     const fpsText = new Text(this.app.ticker.FPS.toFixed(1));
+    const fpsContainer = new Container();
 
-    this.fpsContainer.position = { x: this.width / 2 - fpsText.width - margin, y: this.height / 2 - margin };
-    this.fpsContainer.scale.set(1, -1);
+    fpsContainer.position = { x: this.width / 2 - fpsText.width - margin, y: this.height / 2 - margin };
+    fpsContainer.scale.set(1, -1);
 
     const fpsBackground = new Graphics()
       .beginFill(0x777777, 0.5)
       .drawRect(-margin / 2, -margin / 2, fpsText.width + margin, fpsText.height + margin);
-    this.fpsContainer.addChild(fpsBackground);
-    this.fpsContainer.addChild(fpsText);
-    return this.fpsContainer;
+    fpsContainer.addChild(fpsBackground);
+    fpsContainer.addChild(fpsText);
+
+    this.app.ticker.add(() => this.updateFPS(fpsContainer));
+    return fpsContainer;
   }
 
-  private updateFPS() {
-    const fpsText = this.fpsContainer.getChildAt(1) as Text;
+  private updateFPS(fpsContainer: Container) {
+    const fpsText = fpsContainer.getChildAt(1) as Text;
     fpsText.text = this.app.ticker.FPS.toFixed(1);
   }
 
--- sketches/genuary2023/day01.ts
+++ sketches/genuary2023/day01.ts
@@ -1,33 +1,52 @@
 import { Sketch2D } from "drawing/sketch";
-import { round } from "mathjs";
+import { multiply, round } from "mathjs";
 import { rectanglePacking } from "packing/rectangle";
 import { Rectangle } from "paper";
-import { Container, DisplayObject, Graphics } from "pixi.js";
+import { Container, DisplayObject, Graphics, Rectangle as Rect, RenderTexture, SimplePlane } from "pixi.js";
 
 class Day01 extends Sketch2D {
-  private sideContainer: Container<Graphics>;
-  private loopDurationSeconds = 15;
+  private renderTexture: RenderTexture;
+  private loopDurationSeconds = 10;
 
   constructor(debug = false) {
     super(debug);
-    this.sideContainer = this.generateInfinitePacking();
-    this.sideContainer.position = { x: -this.width / 2, y: -this.height / 2 }; //TODO: Try PIXI.Mesh for perspective mapping
+    this.renderTexture = this.renderer.generateTexture(this.generateInfinitePacking(), {
+      region: new Rect(0, 0, this.width, this.height * 2),
+    });
   }
 
   protected setup(): Container<DisplayObject> {
     const container = new Container();
-    container.addChild(this.sideContainer);
+    const plane = new SimplePlane(this.renderTexture, 100, 100);
+    plane.position = { x: -this.width / 2, y: -this.height / 2 };
+
+    const vertexBuffer = plane.geometry.getBuffer("aVertexPosition");
+    const matrix = [
+      [1, 0, 0],
+      [0, 1, 0],
+      [0, 0, 1],
+    ];
+    const [xFactor, yFactor] = [plane.width, plane.height];
+    for (let i = 0; i < vertexBuffer.data.length; i += 2) {
+      const coords = [vertexBuffer.data[i] / xFactor, vertexBuffer.data[i + 1] / yFactor];
+      const vec3 = [...coords, 1];
+      const transformed = multiply(matrix, vec3);
+      vertexBuffer.data[i] = (transformed[0] * xFactor) / transformed[2];
+      vertexBuffer.data[i + 1] = (transformed[1] * yFactor) / transformed[2];
+    }
+    vertexBuffer.update();
+
+    container.addChild(plane);
     return container;
   }
 
   protected update(deltaTime: number): void {
     const offSetPixels = round((deltaTime * this.height) / this.loopDurationSeconds); //TODO: Google how to do offsets
-    for (const childGraphic of this.sideContainer.children) {
-      childGraphic.position.y -= offSetPixels;
-      if (childGraphic.position.y <= -this.height) {
-        childGraphic.position.y = this.height;
-      }
+    this.renderTexture.frame.y += offSetPixels;
+    if (this.renderTexture.frame.y >= this.height) {
+      this.renderTexture.frame.y -= this.height;
     }
+    this.renderTexture.updateUvs();
   }
 
   private generateInfinitePacking(): Container<Graphics> {
