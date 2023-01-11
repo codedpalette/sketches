1af3922d570ee17ac04184ca8400b8dcaa1e5b57
daniel <daniel.blindr@gmail.com>
2023-01-11 19:49:30 +0200

    Added some todos

--- sketches/genuary2023/day01.ts
+++ sketches/genuary2023/day01.ts
@@ -2,9 +2,10 @@ import { atan, pi } from "mathjs";
 import { rectanglePacking } from "packing/rectangle";
 import { Rectangle } from "paper";
 import {
-  AxesHelper,
-  GridHelper,
+  EdgesGeometry,
   Group,
+  LineBasicMaterial,
+  LineSegments,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
@@ -18,11 +19,11 @@ import { getWebGL2ErrorMessage, isWebGL2Available } from "util/webgl";
 const width = 1080;
 const height = 1080;
 const scene = new Scene();
-scene.add(new AxesHelper());
-scene.add(new GridHelper(1));
+//scene.add(new AxesHelper());
+//scene.add(new GridHelper(1));
 
 //Calculate camera position
-const holeScale = 0.4; //Relation between central hole and screen dimensions
+const holeScale = 0.3; //Relation between central hole and screen dimensions
 const k = (1 - holeScale) / (2 * holeScale);
 const fov = radToDeg(atan(k)) * 2;
 const z = 1 / k + 1;
@@ -30,11 +31,17 @@ const camera = new PerspectiveCamera(fov, width / height, 0.1, z + 1);
 camera.position.setZ(z);
 camera.lookAt(0, 0, 0);
 
-const renderer = new WebGLRenderer({ antialias: true });
+const renderer = new WebGLRenderer({
+  antialias: true,
+  logarithmicDepthBuffer: true, //TODO: animation appear jagged
+  preserveDrawingBuffer: true,
+});
 renderer.setSize(width, height, false);
+renderer.setClearColor("white");
 document.body.appendChild(renderer.domElement);
 
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const material = new MeshBasicMaterial({ color: "lightgreen" });
+const lineMaterial = new LineBasicMaterial({ color: "darkgreen" }); //TODO: lines appear dashed
 const geometry = new PlaneGeometry(1, 1);
 const planes: Group[] = [];
 
@@ -46,9 +53,10 @@ for (let i = 0; i < 4; i++) {
     const group = new Group();
     for (const rect of packing) {
       const rectMesh = new Mesh(geometry, material);
-      rectMesh.scale.setX(rect.width * 0.98);
-      rectMesh.scale.setY(rect.height * 0.98);
+      rectMesh.scale.setX(rect.width);
+      rectMesh.scale.setY(rect.height);
       rectMesh.position.set(rect.center.x - planeWidth / 2, rect.center.y - planeHeight / 2, 0);
+      rectMesh.add(new LineSegments(new EdgesGeometry(rectMesh.geometry), lineMaterial));
       group.add(rectMesh);
     }
     group.position.set(0, 0, 0);
