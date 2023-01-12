fce8ae0964c2eb1b14d9058f3470dba04ae5c1ae
daniel <daniel.blindr@gmail.com>
2023-01-12 08:32:53 +0200

    Rotating camera around axis

--- library/math/angles.ts
+++ library/math/angles.ts
@@ -1,13 +1,11 @@
 import { cos, sin, unit, Unit } from "mathjs";
 
 type Degrees = Unit & { __unit: "deg" };
-type Radians = Unit & { __unit: "rad" };
-type Angle = Degrees | Radians;
 
 export function deg(degrees: number): Degrees {
   return unit(degrees, "deg") as Degrees;
 }
 
-export function fromPolar(r: number, theta: Angle): { x: number; y: number } {
+export function fromPolar(r: number, theta: number | Degrees): { x: number; y: number } {
   return { x: r * cos(theta), y: r * sin(theta) };
 }
--- sketches/genuary2023/day01.ts
+++ sketches/genuary2023/day01.ts
@@ -1,12 +1,10 @@
-import { atan, pi } from "mathjs";
+import { fromPolar } from "math/angles";
+import { atan, pi, tan } from "mathjs";
 import { rectanglePacking } from "packing/rectangle";
 import { Rectangle } from "paper";
 import {
-  BufferGeometry,
   EdgesGeometry,
   Group,
-  LineBasicMaterial,
-  LineSegments,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
@@ -14,7 +12,9 @@ import {
   Scene,
   WebGLRenderer,
 } from "three";
-import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils";
+import { LineMaterial } from "three/examples/jsm/lines/LineMaterial";
+import { LineSegments2 } from "three/examples/jsm/lines/LineSegments2";
+import { LineSegmentsGeometry } from "three/examples/jsm/lines/LineSegmentsGeometry";
 import { radToDeg } from "three/src/math/MathUtils";
 import { getWebGL2ErrorMessage, isWebGL2Available } from "util/webgl";
 
@@ -33,55 +33,62 @@ const camera = new PerspectiveCamera(fov, width / height, 0.1, z + 1);
 camera.position.setZ(z);
 
 const material = new MeshBasicMaterial({ color: "lightgreen", depthWrite: false });
-const lineMaterial = new LineBasicMaterial({ color: "darkgreen" }); //TODO: Lines appear dashed - look at examples
+const lineMaterial = new LineMaterial({ color: 0x006400, linewidth: 0.0025 });
+
 const geometry = new PlaneGeometry(1, 1);
+const edgesGeometry = new LineSegmentsGeometry().fromEdgesGeometry(new EdgesGeometry(geometry));
 const planes: Group[] = [];
 
 const planeWidth = 2;
 const planeHeight = 2;
-for (let i = 0; i < 4; i++) {
+const sides = 4; //TODO: recalculate formulas for dynamic number of slices
+for (let i = 0; i < sides; i++) {
   const packing = rectanglePacking(new Rectangle(0, 0, planeWidth, planeHeight), 20);
-  for (let j = 0; j < 2; j++) {
-    const edgeGeometries: BufferGeometry[] = [];
+  for (let j = 0; j < 3; j++) {
     const group = new Group();
     for (const rect of packing) {
-      const rectMesh = new Mesh(geometry, material);
+      const rectMesh = new Mesh(geometry, material).add(new LineSegments2(edgesGeometry, lineMaterial));
       rectMesh.scale.set(rect.width, rect.height, 1);
       rectMesh.position.set(rect.center.x - planeWidth / 2, rect.center.y - planeHeight / 2, 0);
-
-      edgeGeometries.push(
-        new EdgesGeometry(rectMesh.geometry)
-          .scale(rectMesh.scale.x, rectMesh.scale.y, 1)
-          .translate(rectMesh.position.x, rectMesh.position.y, 0)
-      );
       group.add(rectMesh);
     }
 
-    group.add(new LineSegments(mergeBufferGeometries(edgeGeometries), lineMaterial));
+    const apothem = planeWidth / (2 * tan(pi / sides));
     group
       .rotateX(-pi / 2)
-      .rotateY((pi * i) / 2)
-      .translateZ(-1);
-    if (j) group.position.z -= planeHeight;
+      .rotateY((2 * pi * i) / sides)
+      .translateZ(-apothem);
+    group.position.z -= (j - 1) * planeHeight;
     planes.push(group);
     scene.add(group);
   }
 }
+const backTunnel = new Mesh(geometry, material).add(new LineSegments2(edgesGeometry, lineMaterial));
+backTunnel.position.set(0, 0, -planeHeight / 2);
+backTunnel.scale.set(planeWidth, planeHeight, 1);
+scene.add(backTunnel);
 
 const renderer = new WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
 renderer.setSize(width, height);
-renderer.setClearColor("white");
 document.body.appendChild(renderer.domElement);
 
 const loopDurationSeconds = 5;
+let cameraRotation = 0;
 function animate() {
   requestAnimationFrame(animate);
   const deltaTime = 1 / 60; //60FPS
   const offSet = (deltaTime * planeHeight) / loopDurationSeconds;
+
+  const cameraOffset = (deltaTime * 2 * pi) / loopDurationSeconds;
+  cameraRotation += cameraOffset;
+  const cameraPosition = fromPolar(0.3, cameraRotation);
+  camera.position.set(cameraPosition.x, cameraPosition.y, camera.position.z);
+  camera.rotateZ(cameraOffset / 2);
+
   for (const plane of planes) {
     plane.position.z += offSet;
-    if (plane.position.z >= planeHeight) {
-      plane.position.z -= planeHeight * 2;
+    if (plane.position.z >= 2 * planeHeight) {
+      plane.position.z -= planeHeight * 3;
     }
   }
   renderer.render(scene, camera);
