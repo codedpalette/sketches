afb8daf2494cb979348fbeed11bbf20ac05932c6
daniel <daniel.blindr@gmail.com>
2023-01-14 17:38:49 +0200

    Refactoring file order

--- sketches/S/satellites.ts
+++ sketches/S/satellites.ts
@@ -6,26 +6,74 @@ import { Container, Graphics } from "pixi.js";
 import { random } from "util/random";
 import { init, run } from "drawing/sketch";
 
-class Sun extends Attractor {
-  readonly graphics: Graphics;
+const params = init({ debug: true });
+const system = fromOrbit({
+  position: new Point(0, 0),
+  semiMajor: 200,
+  semiMinor: 150,
+  rotationAngle: 0,
+  periodSeconds: 5,
+});
 
-  constructor(position: Vector2Like, mass: number) {
-    super(position, mass);
-    this.graphics = new Graphics().beginFill(0x000000).drawCircle(this.position.x, this.position.y, 10);
-  }
+const container = new Container();
+container.addChild(system.draw(params.debug));
+const update = (deltaTime: number) => system.update(deltaTime);
+run({ container, update }, params);
+
+function fromOrbit(orbitParams: OrbitParams): PlanetarySystem {
+  const orbit = Orbit.fromParams(orbitParams);
+  const mu = (4 * square(pi) * cube(orbit.semiMajor)) / square(orbit.periodSeconds);
+  const theta = random.real(0, pi * 2);
+
+  const sunPosition = random.pick(orbit.foci);
+  const satellitePosition = [orbit.semiMajor * cos(theta), orbit.semiMinor * sin(theta)] as Vector2;
+  const satelliteVelocity = initialVelocity(orbit, subtract(satellitePosition, sunPosition), theta, mu);
+  return new PlanetarySystem(new Sun(sunPosition, mu), new Satellite(satellitePosition, satelliteVelocity), orbit);
 }
 
-class Satellite extends Mover {
-  readonly graphics: Graphics;
+// https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion#Position_as_a_function_of_time
+function initialVelocity(orbit: Orbit, heliocentricVector: Vector2, theta: number, mu: number): Vector2 {
+  const [r, a, e] = [norm(heliocentricVector) as number, orbit.semiMajor, orbit.eccentricity];
+  const scalar = (sqrt(mu * a) as number) / r;
+  const vector = [-sin(theta), (sqrt(1 - square(e)) as number) * cos(theta)] as Vector2;
+  return multiply(vector, scalar * random.sign());
+}
 
-  constructor(position: Vector2Like, velocity: Vector2Like) {
-    super(position, velocity);
-    this.graphics = new Graphics().beginFill(0xff0000).lineStyle(0).drawCircle(0, 0, 3);
+class PlanetarySystem extends TwoBodySystem {
+  private initialSize: Rectangle;
+  private rectGraphics: Graphics;
+
+  constructor(private sun: Sun, private satellite: Satellite, private orbit: Orbit) {
+    super(sun, satellite);
+
+    this.initialSize = new Rectangle(this.sun.position, this.satellite.position);
+    this.rectGraphics = new Graphics()
+      .beginFill(0x0000ff, 0.5)
+      .drawRect(0, 0, this.initialSize.width, this.initialSize.height);
+    this.rectGraphics.position = this.sun.position;
   }
 
   update(deltaTime: number): void {
     super.update(deltaTime);
-    this.graphics.position = this.position;
+    const scaleVector = this.satellite.position
+      .subtract(this.sun.position)
+      .divide([this.initialSize.width, this.initialSize.height]);
+    this.rectGraphics.scale.set(scaleVector.x, scaleVector.y);
+  }
+
+  draw(debug: boolean): Container {
+    const container = new Container();
+    container.position = this.orbit.position;
+    container.angle = this.orbit.rotationAngle;
+
+    container.addChild(this.rectGraphics);
+    if (debug) {
+      this.orbit.shape.strokeColor = new Color("red");
+      container.addChild(this.sun.graphics);
+      container.addChild(this.satellite.graphics);
+      container.addChild(drawPath(this.orbit.shape));
+    }
+    return container;
   }
 }
 
@@ -60,73 +108,25 @@ class Orbit implements OrbitParams {
   }
 }
 
-class PlanetarySystem extends TwoBodySystem {
-  private initialSize: Rectangle;
-  private rectGraphics: Graphics;
+class Satellite extends Mover {
+  readonly graphics: Graphics;
 
-  private constructor(private sun: Sun, private satellite: Satellite, private orbit: Orbit) {
-    super(sun, satellite);
-
-    this.initialSize = new Rectangle(this.sun.position, this.satellite.position);
-    this.rectGraphics = new Graphics()
-      .beginFill(0x0000ff, 0.5)
-      .drawRect(0, 0, this.initialSize.width, this.initialSize.height);
-    this.rectGraphics.position = this.sun.position;
-  }
-
-  static fromOrbit(orbitParams: OrbitParams): PlanetarySystem {
-    const orbit = Orbit.fromParams(orbitParams);
-    const mu = (4 * square(pi) * cube(orbit.semiMajor)) / square(orbit.periodSeconds);
-    const theta = random.real(0, pi * 2);
-
-    const sunPosition = random.pick(orbit.foci);
-    const satellitePosition = [orbit.semiMajor * cos(theta), orbit.semiMinor * sin(theta)] as Vector2;
-    const satelliteVelocity = this.initialVelocity(orbit, subtract(satellitePosition, sunPosition), theta, mu);
-    return new PlanetarySystem(new Sun(sunPosition, mu), new Satellite(satellitePosition, satelliteVelocity), orbit);
-  }
-
-  // https://en.wikipedia.org/wiki/Kepler%27s_laws_of_planetary_motion#Position_as_a_function_of_time
-  private static initialVelocity(orbit: Orbit, heliocentricVector: Vector2, theta: number, mu: number): Vector2 {
-    const [r, a, e] = [norm(heliocentricVector) as number, orbit.semiMajor, orbit.eccentricity];
-    const scalar = (sqrt(mu * a) as number) / r;
-    const vector = [-sin(theta), (sqrt(1 - square(e)) as number) * cos(theta)] as Vector2;
-    return multiply(vector, scalar * random.sign());
+  constructor(position: Vector2Like, velocity: Vector2Like) {
+    super(position, velocity);
+    this.graphics = new Graphics().beginFill(0xff0000).lineStyle(0).drawCircle(0, 0, 3);
   }
 
   update(deltaTime: number): void {
     super.update(deltaTime);
-    const scaleVector = this.satellite.position
-      .subtract(this.sun.position)
-      .divide([this.initialSize.width, this.initialSize.height]);
-    this.rectGraphics.scale.set(scaleVector.x, scaleVector.y);
-  }
-
-  draw(debug: boolean): Container {
-    const container = new Container();
-    container.position = this.orbit.position;
-    container.angle = this.orbit.rotationAngle;
-
-    container.addChild(this.rectGraphics);
-    if (debug) {
-      this.orbit.shape.strokeColor = new Color("red");
-      container.addChild(this.sun.graphics);
-      container.addChild(this.satellite.graphics);
-      container.addChild(drawPath(this.orbit.shape));
-    }
-    return container;
+    this.graphics.position = this.position;
   }
 }
 
-const params = init({ debug: true });
-const system = PlanetarySystem.fromOrbit({
-  position: new Point(0, 0),
-  semiMajor: 200,
-  semiMinor: 150,
-  rotationAngle: 0,
-  periodSeconds: 5,
-});
+class Sun extends Attractor {
+  readonly graphics: Graphics;
 
-const container = new Container();
-container.addChild(system.draw(params.debug));
-const update = (deltaTime: number) => system.update(deltaTime);
-run({ container, update }, params);
+  constructor(position: Vector2Like, mass: number) {
+    super(position, mass);
+    this.graphics = new Graphics().beginFill(0x000000).drawCircle(this.position.x, this.position.y, 10);
+  }
+}
