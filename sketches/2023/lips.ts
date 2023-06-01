2b7888ad4e6c1d5652b87f4b79c9ff16131fc156
daniel <daniel.blindr@gmail.com>
2023-06-01 18:40:56 +0300

    lips more 3d

--- sketches/2023/lips.ts
+++ sketches/2023/lips.ts
@@ -2,8 +2,8 @@ import { drawPath, setBackground } from "drawing/pixi";
 import { run } from "drawing/sketch";
 import { fromPolar } from "geometry/angles";
 import { Color, plot } from "geometry/paths";
-import { exp, hypot, pi, pow, sqrt } from "mathjs";
-import { Container, Graphics, IPointData } from "pixi.js";
+import { abs, exp, hypot, pi, pow, sqrt } from "mathjs";
+import { Container, Graphics, IPointData, NoiseFilter } from "pixi.js";
 import { map } from "util/map";
 import { noise2d, random } from "util/random";
 
@@ -17,9 +17,9 @@ run((params) => {
   const lower = lip(random.real(0.5, 1), random.real(0, 0.5), false);
 
   const backHue = random.real(0, 360);
-  const background = { h: backHue, s: random.real(10, 30), v: random.real(70, 90) }; //TODO: paper texture
-  const container = new Container();
-  setBackground(container, background, params);
+  const backColor = { h: backHue, s: random.real(10, 30), v: random.real(70, 90) }; //TODO: paper texture - https://www.youtube.com/watch?v=MEYdsoZua7E
+  const backContainer = new Container();
+  setBackground(backContainer, backColor, params);
 
   for (let i = 0; i < 10; i++) {
     const [x, y] = [
@@ -30,8 +30,12 @@ run((params) => {
     const radius = random.real(boundsDiagonal * 0.25, boundsDiagonal * 0.5);
     const color = { h: backHue, s: random.real(10, 30), v: random.real(70, 90), a: random.real(0.3, 0.7) };
     const numVertices = random.integer(5, 15);
-    container.addChild(new Graphics().beginFill(color).drawPolygon(randomPolygon(numVertices, radius, { x, y })));
+    backContainer.addChild(new Graphics().beginFill(color).drawPolygon(randomPolygon(numVertices, radius, { x, y })));
   }
+
+  backContainer.filters = [new NoiseFilter(random.real(0.1, 0.5))];
+  const container = new Container();
+  container.addChild(backContainer);
 
   //TODO: Plot graphs and axes
 
@@ -43,26 +47,33 @@ run((params) => {
       continue;
     }
     const yNorm = y / (y > 0 ? upper(x) : lower(x));
-    const n = noise(x, y);
+    const n = noise(x * scaleFactor, y * scaleFactor);
     const center = { x: x * scaleFactor, y: y * scaleFactor };
     const radius = map(pow(yNorm, 1 / 3), 0, 1, 15, 30);
-    const sat = map(n, 0, 1, 90, 100);
-    const val = map(pow(yNorm, 1 / 2), 0, 1, 50, 100);
-    const alpha = random.real(0.5, 1);
-    const color = { h: 0, s: sat, v: val, a: alpha };
+    const color = {
+      h: 0,
+      s: map(n, 0, 1, 80, 100),
+      v: map(abs(yNorm - 0.5) * 2, 0, 1, 100, 50),
+      a: random.real(0.5, 1),
+    };
     const numVertices = random.integer(3, 9);
-    container.addChild(new Graphics().beginFill(color).drawPolygon(randomPolygon(numVertices, radius, center)));
+    container.addChild(
+      new Graphics()
+        .beginFill(color)
+        .lineStyle(random.real(0.3, 0.7), "black", random.real(0, 0.5))
+        .drawPolygon(randomPolygon(numVertices, radius, center))
+    );
   }
 
   const lips = [upper, lower].map((f) => plot(f, -xBound, xBound));
   lips.forEach((graph) => {
     graph.scale(scaleFactor, [0, 0]);
-    graph.strokeWidth = 5;
+    graph.strokeWidth = 3;
     graph.dashArray = Array.from({ length: random.integer(3, 7) }, (_) => random.integer(10, 40));
-    graph.strokeColor = new Color("hsl(0deg, 100%, 30%");
+    graph.strokeColor = new Color("black");
     const graphics = drawPath(graph);
     graphics.alpha = 0.5;
-    //container.addChild(graphics);
+    params.debug && container.addChild(graphics);
   });
 
   function randomPolygon(numVertices: number, radius: number, center: IPointData): IPointData[] {
