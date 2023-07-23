59211e8779ba408e586133873cb9ff1e1e96fa9d
daniel <daniel.blindr@gmail.com>
2023-07-23 19:11:44 +0300

    Made vortices more "vorticy"

--- sketches/2023/vortices.ts
+++ sketches/2023/vortices.ts
@@ -14,7 +14,7 @@ run((params) => {
 
   const baseTheta = random.real(0, 2 * pi);
   const baseHue = random.real(0, 360);
-  const numVortices = 1;
+  const numVortices = 3;
   const hypotenuse = hypot(params.width / 2, params.height / 2);
   for (let i = 0; i < numVortices; i++) {
     const hue = (baseHue + (360 * i) / numVortices + random.real(-10, 10)) % 360;
@@ -22,37 +22,45 @@ run((params) => {
     const r = random.real(hypotenuse / 4, hypotenuse / 2);
     const theta = (2 * pi * i) / numVortices + baseTheta;
     const { x, y } = fromPolar(r, theta);
-    //vortex.position.set(x, y);
+    //TODO: Composition
+    vortex.position.set(x, y);
   }
 
   return { container };
 
-  //TODO: Try bezier curves for particle trails
-
   function drawVortex(hue: number) {
     const particleCount = random.real(200, 300);
     const maxRadius = random.real(400, 500);
-    const minRadius = 10;
-    const thetaStep = 0.01;
-    const trailLength = 50;
-    const valPeriod = 100;
+    const minRadius = 30;
+    const thetaStep = 0.001;
+    const trailLength = 1000;
+    const valPeriod = 10;
 
     const sat = random.real(25, 100);
     const val = random.real(50, 100);
-    const g = new Graphics();
-    for (let i = 0; i < particleCount; i++) {
-      const r = map(pow(random.realZeroToOneInclusive(), 1 / 2), 0, 1, minRadius, maxRadius);
-      const theta = random.real(0, 2 * pi);
-      const length = trailLength;
 
-      const valTheta = map((r - minRadius) % valPeriod, 0, valPeriod, 0, 2 * pi);
+    const vortexCoreRadius = maxRadius / 4;
+    const gamma = random.real(0.03, 0.09);
+    const c = new Container();
+    for (let i = 0; i < particleCount; i++) {
+      let r = map(random.realZeroToOneInclusive(), 0, 1, minRadius, maxRadius);
+      let theta = random.real(0, 2 * pi);
+
+      const valTheta = map((r - minRadius) % valPeriod, 0, valPeriod, 0, 2 * pi); //TODO: Color variations
       const valOffset = map(abs(sin(valTheta)), 0, 1, 50, 0); //cos(valTheta) * 25;
-      for (let j = 0; j < length; j++) {
-        const { x, y } = fromPolar(r, theta + j * thetaStep);
+
+      const g = c.addChild(new Graphics());
+      for (let j = 0; j < trailLength; j++) {
+        const rotationalVelocity = (gamma / (2 * pi)) * (r <= vortexCoreRadius ? r / pow(vortexCoreRadius, 2) : 1 / r);
+        const { x, y } = fromPolar(r, theta);
         const color = hsv.hex([hue, sat, val - valOffset]);
-        g.beginFill(color, 0.5).drawCircle(x, y, 0.5 + n(j, i * 100) + i / length);
+        const lineThickness = (1 + n(i * 100, j) + j / trailLength) * 2;
+        g.lineStyle(lineThickness, color, random.real(0.5, 1));
+        j == 0 ? g.moveTo(x, y) : g.lineTo(x, y);
+        theta += thetaStep;
+        r -= rotationalVelocity / thetaStep;
       }
     }
-    return g;
+    return c;
   }
 });
