bdb134672e423f13f519d5b7055c920b54d9044b
daniel <daniel.blindr@gmail.com>
2023-09-10 00:40:30 +0300

    Added comment with a link to glsl-loader docs

--- typings/glsl-files.d.ts
+++ typings/glsl-files.d.ts
@@ -1,3 +1,4 @@
+// https://usegpu.live/docs/reference-loader-@use-gpu-glsl-loader
 declare module "*.glsl" {
   type ParsedBundle = import("@use-gpu/shader/types").ParsedBundle
   const __module: ParsedBundle
