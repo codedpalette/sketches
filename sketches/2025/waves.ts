62f4986f18e5b26906f2c2866e06f60212c99bbc
daniel <daniel.blindr@gmail.com>
2025-03-27 19:21:42 +0200

    Refactor

--- sketches/2025/waves.ts
+++ sketches/2025/waves.ts
@@ -14,39 +14,49 @@ export default (p: p5) => {
     p.redraw()
   }
 
-  const waveSpace = 0.05
-  const waveFrequency = 10
-
   p.draw = () => {
+    const waveSpacing = 0.05
     p.scale(p.width / 2, -p.height / 2)
+    // TODO: Mask background
+    drawWaveBackground()
+    p.stroke(0, 50)
+    p.fill("#203A46")
+
+    let y = 0
+    while (y >= -1 - waveSpacing) {
+      const waveAmplitude = p.map(y, -1, 0, waveSpacing, waveSpacing * 0.1)
+      const wavePoints = p.map(y, -1, 0, 100, 10)
+      const noiseScale = p.map(y, -1, 0, 1, 100)
+      const noiseOffset = 0.5 //p.map(y, -1, 0, 0, 0.5)
+      p.strokeWeight(1 + Math.abs(y) * 4)
+      drawWave(y, waveAmplitude, wavePoints, noiseScale, noiseOffset)
+      y -= waveAmplitude
+    }
+    //p.filter("blur", 1)
+  }
+
+  function drawWave(yStart: number, amplitude: number, numPoints: number, noiseScale: number, noiseOffset: number) {
+    p.beginShape()
+    const pointStep = 2 / numPoints
+    p.curveVertex(-1, yStart)
+    p.curveVertex(-1, yStart)
+    for (let i = 0; i <= numPoints; i++) {
+      const x = -1 + i * pointStep
+      const noise = (p.noise((x + 1) * noiseScale, yStart * noiseScale) - noiseOffset) * amplitude * 2
+      const y = yStart + noise // TODO: Add curvature
+      p.curveVertex(x, y)
+    }
+    p.curveVertex(1, yStart)
+    p.curveVertex(1, yStart)
+    p.endShape()
+  }
+
+  function drawWaveBackground() {
     p.noStroke()
     p.fill("black")
     p.rect(-1, 0, 2, -1)
     p.fill("white")
     p.triangle(0, 0.25, -0.5, -1, 0.5, -1)
-    p.filter("blur", 50)
-
-    p.stroke(0)
-    p.fill("#203A46")
-    let y = 0
-    while (y >= -1) {
-      const noiseFactor = 10 //p.map(y, -1, 0, 100, 10)
-      const waveDetail = p.map(y, -1, 0, 0.01, 0.1)
-      const waveAmplitude = p.map(y, -1, 0, waveSpace, waveSpace * 0.1)
-      const freq = p.map(y, -1, 1, waveFrequency, waveFrequency * 40)
-      p.strokeWeight(1 + Math.abs(y) * 4)
-      p.beginShape()
-      for (let x = -1 - waveDetail; x <= 1 + waveDetail * 2; x += waveDetail) {
-        const wave = Math.cos(x * freq) * waveAmplitude
-        const n = (p.noise(x * noiseFactor, y * 100) - 0.5) * 4 * waveAmplitude
-        //const mix = y + 1
-        //const yVertex = y + p.lerp(n, wave, mix)
-        const yVertex = y + n + wave * 0.4
-        p.curveVertex(x, yVertex)
-      }
-      p.endShape()
-      y -= waveAmplitude
-    }
-    p.filter("blur", 0.5)
+    p.filter("blur", 10)
   }
 }
